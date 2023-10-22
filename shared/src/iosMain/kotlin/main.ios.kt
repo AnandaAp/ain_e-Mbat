cb8e7ec11eb418ed69f3ade062cc784da9556d66
AnandaAp <ananda.apriliansyah@ti.ukdw.ac.id>
2023-10-22 18:50:04 +0700

    work of bottom bar
    1. Implement material3
    2. Able to retrieve the value from Collection inside of Collection from Firestore
    3. Bring Harmony to project :D

--- shared/src/iosMain/kotlin/main.ios.kt
+++ shared/src/iosMain/kotlin/main.ios.kt
@@ -1,11 +1,7 @@
-import androidx.compose.material.MaterialTheme
 import androidx.compose.ui.window.ComposeUIViewController
-import ui.theme.Material3AinEmbatTheme
 
 actual fun getPlatformName(): String = "iOS"
 
 fun MainViewController() = ComposeUIViewController {
-    Material3AinEmbatTheme {
-        App()
-    }
+    App()
 }
